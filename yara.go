78ddc3b42d45f0951363fc168d55741ee98424f1
blacktop <blacktop@users.noreply.github.com>
2016-09-25 20:50:03 -0600

    fix imports

--- yara.go
+++ yara.go
@@ -11,8 +11,8 @@ import (
 	"github.com/crackcomm/go-clitable"
 	"github.com/fatih/structs"
 	"github.com/hillu/go-yara"
-	"github.com/maliceio/malice/malice/database/elasticsearch"
-	"github.com/maliceio/malice/utils"
+	"github.com/maliceio/go-plugin-utils/database/elasticsearch"
+	"github.com/maliceio/go-plugin-utils/utils"
 	"github.com/urfave/cli"
 )
 
